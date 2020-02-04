f943e516fe257bf2c3c0d37a37de2f1cf0962d05
Adame koita <adame.koita@gmail.com>
2020-02-04 21:19:05 +0100

    modif form register

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -19,9 +19,111 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $Adresse;
+
+    /**
+     * @ORM\Column(type="number", length=5)
+     */
+    private $postal_code;
+
+    /**
+     * @ORM\Column(type="string", length=25)
+     */
+    private $ville;
+
+    /**
+     * @ORM\Column(type="string", length=10, nullable=true)
+     */
+    private $numero_telephone;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->Adresse;
+    }
+
+    public function setAdresse(string $Adresse): self
+    {
+        $this->Adresse = $Adresse;
+
+        return $this;
+    }
+
+    public function getPostalCode(): ?string
+    {
+        return $this->postal_code;
+    }
+
+    public function setPostalCode( string $postal_code): self
+    {
+        $this->postal_code = $postal_code;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getNumeroTelephone(): ?string
+    {
+        return $this->numero_telephone;
+    }
+
+    public function setNumeroTelephone(?string $numero_telephone): self
+    {
+        $this->numero_telephone = $numero_telephone;
+
+        return $this;
     }
 }
