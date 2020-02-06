958077829d4dfdc8961175735c7ea75f2e43b077
tah-dame <adame.koita@gmail.com>
2020-02-06 15:03:54 +0100

    mailing

--- src/Controller/PirateController.php
+++ src/Controller/PirateController.php
@@ -10,8 +10,16 @@ class PirateController extends AbstractController
     /**
      * @Route("/pirate", name="pirate")
      */
-    public function index()
+    public function index( \Swift_Mailer $mailer)
     {
+        $message = (new \Swift_Message('Hello Email'))
+            ->setFrom('wf3sens2020@gmail/com')
+            ->setTo('wf3sens2020@gmail.com')
+            ->setBody('You should see me from the profiler!')
+        ;
+    
+        $mailer->send($message);
+
         return $this->render('pirate/index.html.twig', [
             'controller_name' => 'PirateController',
         ]);
