0ac9325ab4dc62682c4a365461c54c7b9a08b302
Guillaume Flambard <g.flambard@gmail.com>
2020-02-08 11:44:35 +0100

    changements mineurs footer

--- config/bundles.php
+++ config/bundles.php
@@ -13,5 +13,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
