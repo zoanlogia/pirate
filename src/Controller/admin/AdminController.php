8eec4e94303d4e6956eeea3bb3721daad08f71f7
Guillaume Flambard <g.flambard@gmail.com>
2020-02-12 10:47:24 +0100

    register

--- src/Controller/admin/AdminController.php
+++ src/Controller/admin/AdminController.php
@@ -1,17 +1,20 @@
 <?php
 
-namespace App\Controller\Admin;
+namespace App\Controller\admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-/** @Route("/admin") */
-class HomepageController extends AbstractController {
+/** @Route("/admin")
+ * 
+*/
+class AdminController extends AbstractController {
 
     /**
      * @Route("/")
      */
     public function index() {
+        dump('OKqsqs');die;
         return $this->render('admin/base.html.twig', ['mainNavAdmin' => true, 'title' => 'Espace Admin']);
     }
 
